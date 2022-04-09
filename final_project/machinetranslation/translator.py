e640d3fb833adee143457dbaacc73dbb304ffbe3
Ad1tyaBhardwaj <adityabhardwaj98144@gmail.com>
2022-04-09 09:56:40 +0000

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,44 @@
+'''imported modules
+'''
+
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+print(apikey)
+
+authenticator = IAMAuthenticator('xB4The5OOj0cC7EkHiLZmxlsrEBndIxWwflAVEe7QWLk')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+language_translator.set_service_url('https://api.jp-tok.language-translator.watson.cloud.ibm.com')
+
+'''
+Function to translate english text to french
+'''
+def english_to_french(english_text):
+    french_text = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    french_text = (french_text["translations"][0]['translation'])
+    print(french_text)
+    return french_text
+
+'''
+Function to translate french text to english
+'''
+
+def french_to_english(french_text):
+    english_text = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    english_text = (english_text["translations"][0]['translation'])
+    print(english_text)
+    return english_text
